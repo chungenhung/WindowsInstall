57a722a5de45fd0aad39a9d0b70d2a3f6bced0d8
chungenhung <chungenhung@gmail.com>
2020-08-28 02:44:19 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,7 +4,8 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 	Exit
 }
 
-
 Write-Output "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install chocolatey-core.extension -y
+
+pause
