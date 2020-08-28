59cee08b82486a217db00716e361dfbeb3d803fb
chungenhung <chungenhung@gmail.com>
2020-08-28 02:45:01 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,8 +1,3 @@
-# Relaunch the script with administrator privileges
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
-	Exit
-}
 
 Write-Output "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
