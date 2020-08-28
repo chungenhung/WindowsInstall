01a2d985fe86c799bd7cbbebd2365d1e7b3c02bf
chungenhung <chungenhung@gmail.com>
2020-08-28 02:51:34 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,9 +1,3 @@
-# Relaunch the script with administrator privileges
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
-	Exit
-}
-
 Write-Output "Installing Adobe Acrobat Reader"
 choco install adobereader -y
 
