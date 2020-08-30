2f9b1c7d5e88422dc486f5dc9e6070258b820f84
chungenhung <chungenhung@gmail.com>
2020-08-30 00:14:32 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,6 @@
 $tweaks = @(
 	### Require administrator privileges ###
 	"RequireAdmin"
-	#"InstallPrograms"
 )
 
 # Relaunch the script with administrator privileges
@@ -14,54 +13,6 @@ Function RequireAdmin {
 	}
 	Write-Output "Msg 1."
 	Read-Host -Prompt "Press Enter to continue"
-}
-
-Function InstallPrograms {
-	Write-Output "Msg 2."
-	Read-Host -Prompt "Press Enter to continue"
-	
-	Write-Output "Executing FUnction InstallPrograms"
-	
-	Write-Output "Msg 3."
-	Read-Host -Prompt "Press Enter to continue"
-		
-	Write-Output "Installing Chocolatey"
-	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-	choco install chocolatey-core.extension -y
-
-	Write-Output "Install these Visual C++ Redistributable Packages first. Because some programs require it"
-
-	Write-Output "Microsoft Visual C++ Runtime - all versions 1.0.0"
-	choco install vcredist-all -y
-
-	#This no longer needed. It is already included in the above script.
-	#Write-Output "Installing Visual C++ Redistributable Packages for Visual Studio 2013 12.0.40660.20180427"
-	#choco install vcredist2013 -y
-
-	Write-Output "Installing 7-Zip"
-	choco install 7zip -y
-	
-	Write-Output "Installing Adobe Acrobat Reader"
-	choco install adobereader -y
-	
-	Write-Output "Installing Audacity"
-	choco install audacity -y
-	
-	Write-Output "Installing Brave Browser"
-	choco install brave -y
-	
-	Write-Output "Pause 10 seconds for Brave Browser to start"
-	Start-Sleep -Seconds 10
-	
-	Write-Output "Closing Brave Browser"
-	Stop-Process -Name "Brave" -Force
-	
-	#Write-Output "Removing Brave Browser desktop shortcut"
-	#Remove-Item -path $home\Desktop\Brave.lnk
-
-	Write-Output "Removing ALL desktop shortcuts"
-	Remove-Item -path C:\Users\Public\Desktop\*.lnk
-	Remove-Item -path $home\Desktop\*.lnk
 }
 
 ##########
@@ -88,5 +39,17 @@ If ($args) {
 # Call the desired tweak functions
 $tweaks | ForEach { Invoke-Expression $_ }
 
+Write-Output "Msg 2."
+Read-Host -Prompt "Press Enter to continue"
+
+Write-Output "Executing FUnction InstallPrograms"
+	
+Write-Output "Msg 3."
+Read-Host -Prompt "Press Enter to continue"
+		
+Write-Output "Installing Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install chocolatey-core.extension -y
+	
 Write-Output "Msg 4."
 Read-Host -Prompt "Press Enter to continue"
