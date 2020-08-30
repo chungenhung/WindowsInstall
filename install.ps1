2adc3703aae4b615fd53f814866650fe01f2156e
chungenhung <chungenhung@gmail.com>
2020-08-29 23:23:44 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -25,8 +25,9 @@ Function RequireAdmin {
 }
 
 Function InstallPrograms {
+	pause
 	Write-Output "Executing FUnction InstallPrograms"
-
+	pause
 	Write-Output "Installing Chocolatey"
 	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 	choco install chocolatey-core.extension -y
