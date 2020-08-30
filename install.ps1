f99728b09a614a82fce372ed55d7c9ceeac96883
chungenhung <chungenhung@gmail.com>
2020-08-29 23:32:04 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,14 +1,3 @@
-<#
-https://christitus.com/ultimate-windows-setup-guide/
-The original URL is https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/win10debloat.ps1
-He shortened it using git.io.
-powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JJ8R4')"
-
-#My own script
-powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/chungenhung/WindowsInstall/master/install.ps1')"
-#>
-#===============================================================================
-
 #List of functions that will run.
 $tweaks = @(
 	### Require administrator privileges ###
@@ -25,29 +14,18 @@ Function RequireAdmin {
 }
 
 Function InstallPrograms {
-	pause
+	Read-Host -Prompt "Press Enter to continue"
+	
 	Write-Output "Executing FUnction InstallPrograms"
-	pause
+	
+	Read-Host -Prompt "Press Enter to continue"
+	
 	Write-Output "Installing Chocolatey"
 	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 	choco install chocolatey-core.extension -y
 
 	Write-Output "Install these Visual C++ Redistributable Packages first. Because some programs require it"
-	<#
-	This includes:
-	- vcredist2012 v11.0.61031
-	- kb3033929 v1.0.5
-	- kb2999226 v1.0.20181019
-	- vcredist2005 v8.1.0.20160118
-	- vcredist2015 v14.0.24215.20170201
-	- vcredist2008 v9.0.30729.6163
-	- vcredist2010 v10.0.40219.2
-	- vcredist-all v1.0.0
-	- vcredist2017 v14.16.27033
-	- vcredist140 v14.26.28720.3
-	- kb3035131 v1.0.3
-	- chocolatey-windowsupdate.extension v1.0.4
-	#>
+
 	Write-Output "Microsoft Visual C++ Runtime - all versions 1.0.0"
 	choco install vcredist-all -y
 
@@ -79,13 +57,6 @@ Function InstallPrograms {
 	Write-Output "Removing ALL desktop shortcuts"
 	Remove-Item -path C:\Users\Public\Desktop\*.lnk
 	Remove-Item -path $home\Desktop\*.lnk
-<#
-As of 2020-08-28. These programs are NOT available on chocolatey.org:
-Duck Capture
-Logitech G-Hub. For Logitech gaming keyboard and gaming headphones.
-Microsoft Office. Chocolatey have various versions, but not the one I think I need.
-#>
-
 }
 
 ##########
@@ -112,15 +83,4 @@ If ($args) {
 # Call the desired tweak functions
 $tweaks | ForEach { Invoke-Expression $_ }
 
-pause
-
-<#
-Privacy settings to disable:
-Diagnostic data
-Find my device
-Inking & typing
-Location
-Online speech recognition
-Tailored experiences
-Advertising ID
-#>
+Read-Host -Prompt "Press Enter to continue"
